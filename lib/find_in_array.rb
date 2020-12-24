083ebbc63d44950fb4750d00a160dd1b396cc546
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-24 20:57:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,10 +5,8 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while count < array.length do
     if array[count] == value_to_find
-      found_value_index = count
+      return count 
     end
     count += 1
   end
-  found_value_index
-  binding.pry
 end
